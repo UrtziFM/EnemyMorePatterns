9651d83cc49e0b92b34b8d658cae7e0de5bdc53f
u100247 <u100247@MM0012B7.local>
2022-11-26 12:47:35 +0100

    Canvas done, time 1:30

---
+++ App.js
@@ -0,0 +1,12 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvasWidth = canvas.width = 500;
+canvasHeight = canvas.height = 1000;
+
+enemy1 = {
+    x: 0,
+    y: 0,
+    width: 200,
+    height: 200,
+}
